d7699cfa9105d889b3346689202f957bef33c918
Ferdinald Dogbey <ferdinald.dogbey@amalitech.com>
2024-11-15 17:08:13 +0000

    initial files

---
+++ Objects.js
@@ -0,0 +1,25 @@
+class Person {
+    constructor(name, age) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        if (typeof age !== 'number') {
+            throw new TypeError('Age must be a number');
+        }
+        this.name = name;
+        this.age = age;
+
+        this.greet = function() {
+            console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+        };
+    }
+}
+
+const person1 = new Person('Ferdinald', 34);
+const person2 = { name: 'Alice', age: 28 };
+//person1.greet.call(person2);  
+
+//const boundGreet = person1.greet.bind(person2);
+//boundGreet();  
+
+person1.greet.apply(person2);  
